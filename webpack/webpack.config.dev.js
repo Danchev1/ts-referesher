d250f6ff5950e76e5c647c2f0fcedb002eab0264
yordandanchev <yordangdanchev@gmail.com>
2020-05-07 22:58:23 +0300

    initial setup - jest --watch and --coverage added

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,27 +1,27 @@
 const dir = require('./utils/dir');
 
 module.exports = {
-  mode: 'development',
-  entry: dir.entryPath,
-  output: {
-    filename: 'app.js',
-    path: dir.distPath,
-    publicPath: 'dist'
-  },
-  devServer: {
-    port: 8888,
-    contentBase: dir.distPath,
-  },
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.ts?$/,
-        use: ['babel-loader', 'eslint-loader']
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.ts', '.js' ],
-  }
+    mode: 'development',
+    entry: dir.entryPath,
+    output: {
+        filename: 'app.js',
+        path: dir.distPath,
+        publicPath: 'dist'
+    },
+    devServer: {
+        port: 8888,
+        contentBase: dir.distPath
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: ['babel-loader', 'eslint-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    }
 };
--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -3,28 +3,28 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const dir = require('./utils/dir');
 
 module.exports = {
-  mode: 'production',
-  entry: dir.entryPath,
-  output: {
-    filename: 'app.js',
-    path: dir.distPath
-  },
-  devtool: false,
-  module: {
-    rules: [
-      {
-        test: /\.ts?$/,
-        use: ['babel-loader', 'eslint-loader']
-      },
+    mode: 'production',
+    entry: dir.entryPath,
+    output: {
+        filename: 'app.js',
+        path: dir.distPath
+    },
+    devtool: false,
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: ['babel-loader', 'eslint-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    plugins: [
+        new CleanWebpackPlugin.CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: dir.indexHtmlPath
+        })
     ]
-  },
-  resolve: {
-    extensions: ['.ts', '.js' ],
-  },
-  plugins: [
-      new CleanWebpackPlugin.CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-        template: dir.indexHtmlPath
-      })
-  ]
 };
